089ff8e5c3c309e52769858f4ae26a581d127535
Usuario <Usuario@PC16>
2022-11-16 13:14:01 +0100

    Se crea el atributo cadenaAMostrar

---
+++ src/main/java/modelo/Color.java
@@ -0,0 +1,16 @@
+package modelo;
+
+public enum Color {
+	BLANCO ("Blanco"),
+	NEGRO ("Negro"),
+	
+	private String cadenaAMostrar;
+	private Color(String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+		}
+	@Override
+	public String toString() {
+		return cadenaAMostrar;
+	}
+}
+
